26f198297c899449a147a07c996be40879a869c8
errowdrigorena <xxlgran@gmail.com>
2022-08-14 16:09:26 +0200

    erase warnings. Split .cpp and .h

--- src/ServerOxana.cpp
+++ src/ServerOxana.cpp
@@ -54,46 +54,6 @@ using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
 int main(int argc, char* argv[])
 {
-    // Check command line arguments.
-    /*if (argc != 5)
-    {
-        std::cerr <<
-            "Usage: http-server-async-ssl <address> <port> <doc_root> <threads>\n" <<
-            "Example:\n" <<
-            "    http-server-async-ssl 0.0.0.0 8080 . 1\n";
-        return EXIT_FAILURE;
-    }*/
-    auto const address = net::ip::make_address("127.0.0.1");
-    auto const port = static_cast<unsigned short>(8080);
-    auto const doc_root = std::make_shared<std::string>("./");
-    auto const threads = 4;
-
-    // The io_context is required for all I/O
-    net::io_context ioc{threads};
-
-    // The SSL context is required, and holds certificates
-    ssl::context ctx{ssl::context::tlsv12};
-
-    // This holds the self-signed certificate used by the server
-    load_server_certificate(ctx);
-
-    // Create and launch a listening port
-    /*std::make_shared<listener>(
-        ioc,
-        ctx,
-        tcp::endpoint{address, port},
-        doc_root)->run();
-
-    // Run the I/O service on the requested number of threads
-    std::vector<std::thread> v;
-    v.reserve(threads - 1);
-    for(auto i = threads - 1; i > 0; --i)
-        v.emplace_back(
-        [&ioc]
-        {
-            ioc.run();
-        });
-    ioc.run();*/
 	run_server();
 
 
